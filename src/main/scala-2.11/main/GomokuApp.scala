336cc1c1bd9dd5ad6e79ce326f0f2774cbf84e20
PhilipKazmeier <kazmeierphilip@gmail.com>
2015-06-10 14:22:10 +0200

    stuff

--- src/main/scala-2.11/main/GomokuApp.scala
+++ src/main/scala-2.11/main/GomokuApp.scala
@@ -12,6 +12,8 @@ object GomokuApp extends JFXApp {
   val WELCOME = "Welcome to Gomoku!"
   val WELCOME_MESSAGE = "Gomoku is a game for two suckers that are bored. FUCK YOU!"
 
+  val controller = new GameController
+
   stage = new PrimaryStage{
     title.value = "Gomoku"
     width = 500
@@ -19,15 +21,14 @@ object GomokuApp extends JFXApp {
     val stackPane = new StackPane()
     scene = new Scene {
       content = new AnchorPane{
-        val boardPane = new Board()
-        val setupPane = new SetupPane()
+        val boardPane = new Board(controller)
+        val setupPane = new SetupPane(controller)
         val statusPane = new StatusPane()
         stackPane.children.add(boardPane)
         stackPane.children.add(setupPane)
       }
     }
   }
-  val controller = new GameController
   start()
 
   def start() = {
--- src/main/scala-2.11/view/SetupPane.scala
+++ src/main/scala-2.11/view/SetupPane.scala
@@ -1,12 +1,14 @@
 package view
 
 
+import control.GameController
 import main.GomokuApp
 
 import scalafx.scene.control.{Slider, Label, Button, TextField}
 import scalafx.scene.layout.AnchorPane
+import scalafx.Includes._
 
-class SetupPane extends AnchorPane {
+class SetupPane(gameController: GameController) extends AnchorPane {
   prefHeight = 250
   prefWidth = 300
 
@@ -17,12 +19,19 @@ class SetupPane extends AnchorPane {
   val playerNameTwo = new TextField
   val sizeSlider = new Slider{
     majorTickUnit = 1
+    minorTickCount = 1
     min = 5
     max = 20
   }
 
   val submit = new Button{
     text = "Submit"
+    onAction = handle{
+      gameController.createPlayer(0, playerNameOne.text.value)
+      gameController.createPlayer(1, playerNameTwo.text.value)
+      gameController.createTable(sizeSlider.value.value.toInt)
+      gameController.start
+    }
   }
 
 }
