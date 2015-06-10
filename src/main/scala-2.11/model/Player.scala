3d4ceda81086cbf4631c52434e01841ae2464a99
Sven <sven.s.mayer@gmx.de>
2015-06-10 13:01:48 +0200

    Added Player vals

--- src/main/scala-2.11/model/Player.scala
+++ src/main/scala-2.11/model/Player.scala
@@ -6,5 +6,8 @@ class Player(val name: String, val icon: Char) {
     require(name != null, "Players need a name")
     require(icon != null, "Players need a icon")
 
+    val WON = s"$name won the game."
+    val ROUND = s"It's $name's turn."
+
     def placeSymbolOnTable(table:Table) = ???
 }
