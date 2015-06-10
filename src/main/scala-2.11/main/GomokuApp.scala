7f602ed768c9046e060356dee7a875e3ff459ae0
CallMeBW <usRfish@gmail.com>
2015-06-10 13:16:33 +0200

    AnchorPanes

--- src/main/scala-2.11/main/GomokuApp.scala
+++ src/main/scala-2.11/main/GomokuApp.scala
@@ -1,26 +1,35 @@
 package main
 
 import control.GameController
+import view.{SetupPane, StatusPane, Board}
 
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.scene.Scene
+import scalafx.scene.layout.{StackPane, AnchorPane}
 
 object GomokuApp extends JFXApp {
   stage = new PrimaryStage{
     title.value = "Gomoku"
     width = 500
     height = 500
+    val stackPane = new StackPane()
     scene = new Scene {
-
+      content = new AnchorPane{
+        val boardPane = new Board()
+        val setupPane = new SetupPane()
+        val statusPane = new StatusPane()
+        stackPane.children.add(boardPane)
+        stackPane.children.add(setupPane)
+      }
     }
   }
   val controller = new GameController
   start()
 
   def start() = {
-    controller.createPlayers
-    ???
+//    controller.createPlayers
+    stage.show()
   }
 
 
---
+++ src/main/scala-2.11/view/Board.scala
@@ -0,0 +1,6 @@
+package view
+
+import scalafx.scene.layout.AnchorPane
+
+class Board extends AnchorPane {
+}
