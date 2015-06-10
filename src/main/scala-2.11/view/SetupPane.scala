fea8619d175dc7260cea9bf435301dbf19856553
PhilipKazmeier <kazmeierphilip@gmail.com>
2015-06-10 14:03:35 +0200

    implemented parts of the setup pane

--- src/main/scala-2.11/view/SetupPane.scala
+++ src/main/scala-2.11/view/SetupPane.scala
@@ -3,15 +3,26 @@ package view
 
 import main.GomokuApp
 
-import scalafx.scene.control.{Label, TextField}
+import scalafx.scene.control.{Slider, Label, Button, TextField}
 import scalafx.scene.layout.AnchorPane
 
 class SetupPane extends AnchorPane {
   prefHeight = 250
   prefWidth = 300
+
   val welcomeLabel = new Label{
     text = GomokuApp.WELCOME
   }
   val playerNameOne = new TextField
   val playerNameTwo = new TextField
+  val sizeSlider = new Slider{
+    majorTickUnit = 1
+    min = 5
+    max = 20
+  }
+
+  val submit = new Button{
+    text = "Submit"
+  }
+
 }
