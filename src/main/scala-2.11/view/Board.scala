cced43ca7159c6592347bab35535d6cd601e09af
Sven <sven.s.mayer@gmx.de>
2015-06-10 14:59:29 +0200

    Board

--- src/main/scala-2.11/view/Board.scala
+++ src/main/scala-2.11/view/Board.scala
@@ -2,8 +2,15 @@ package view
 
 import control.GameController
 
-import scalafx.scene.layout.AnchorPane
+import scalafx.scene.layout.{Pane, GridPane, AnchorPane}
 
 class Board(gameController: GameController) extends AnchorPane {
+
+  val gridPane = new GridPane();
+  gridPane.setHgap(5);
+  gridPane.setVgap(5);
+
+
+
 
 }
